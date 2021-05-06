4f6c90c3542858a1b3ef34eb8c70a92b8126718a
Luan de Almeida da Silva <luanalmeida3976@gmail.com>
2021-05-06 13:49:10 -0300

    js

---
+++ script/game.js
@@ -0,0 +1,14 @@
+// iniciar minhas variaveis
+let board = [ '','','','','','','','',''];
+let playerTime = 0;
+let symbols = ['o','x'];
+
+function handleMove(position){
+    bord[position] = symbols[playerTime];
+    if(playerTime == 0){
+        playerTime = 1
+    }else{
+        playerTime = 0
+    }
+
+}
